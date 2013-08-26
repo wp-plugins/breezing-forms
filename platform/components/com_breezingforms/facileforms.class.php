5dee30a46bc1cefd365ea8f7d236d6398c2bbfcd
crosstec <crosstec@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-26 22:08:12 +0000

    * Fixed an issue with footer display (missing data) * Fixed php strict mode issues * Refurbished theme engine for better responsiveness
    git-svn-id: https://plugins.svn.wordpress.org/breezing-forms/trunk@762640 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- platform/components/com_breezingforms/facileforms.class.php
+++ platform/components/com_breezingforms/facileforms.class.php
@@ -10,7 +10,7 @@ defined( '_JEXEC' ) or die( 'Direct Access to this location is not allowed.' );
 
 global $ff_version, $ff_resnames, $ff_request, $ff_target;
 
-$ff_version = '1.2.6 Stable (build 237)';
+$ff_version = '1.2.7 Stable (build 238)';
 $ff_target  = 0;
 
 $ff_resnames = array(
